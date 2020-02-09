1b9f285ae91741470ab36a79c58cca2b3af13168
Joao Victor <vitorufc2013@gmail.com>
2020-02-08 21:34:57 -0300

    projeto adicionado

---
+++ Pokemon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pokemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
