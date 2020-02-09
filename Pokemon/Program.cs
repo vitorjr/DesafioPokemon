f09d15340378561c6366632159879677d545504b
Joao Victor <vitorufc2013@gmail.com>
2020-02-08 22:35:05 -0300

    Adicionando informacoes basicas

--- Pokemon/Program.cs
+++ Pokemon/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using HtmlAgilityPack;
 
 namespace Pokemon
 {
@@ -6,7 +8,22 @@ namespace Pokemon
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("1 - Acesse o site https://www.pokemon.com/us/pokemon-tcg/pokemon-cards/");
+            Console.WriteLine("2 - Realize uma pesquisa sem preencher nenhum campo(Clicando em Search)");
+            Console.WriteLine("3 - Informe abaixo a quantidade de páginas:");
+            int numero_paginas = int.Parse(Console.ReadLine());
+
+            
+            var wc = new WebClient();
+            string pagina = wc.DownloadString("https://www.pokemon.com/us/pokemon-tcg/pokemon-cards/?cardName=&cardText=&evolvesFrom=&simpleSubmit=&format=unlimited&hitPointsMin=0&hitPointsMax=340&retreatCostMin=0&retreatCostMax=5&totalAttackCostMin=0&totalAttackCostMax=5&particularArtist=");
+
+            var htmlDocument = new HtmlAgilityPack.HtmlDocument();
+            htmlDocument.LoadHtml(pagina);
+
+            string url_imagem = string.Empty;
+            string titulo = string.Empty;
+
+
         }
     }
 }
